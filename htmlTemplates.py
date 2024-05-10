7e545502e17df480809e9939f12a73b524c1e532
Hyunsoo Shin <2091126@hansung.ac.kr>
2024-05-10 19:38:29 +0900

    Fix error.
    PdfToQuiz 에서 htmlTemplates.py 가 없어서 에러 발생, 각종 라이브러리가 없어서 에러 발생, 오픈 api 키가 없어서 에러 발생

---
+++ htmlTemplates.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://static.thenounproject.com/png/5862232-200.png">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThL_-PeQELTCgDcMrx7NnHcHB3TpNdlRwO-f6QB7-c8e9_Uh0W2H2H7mEcAg&s">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
