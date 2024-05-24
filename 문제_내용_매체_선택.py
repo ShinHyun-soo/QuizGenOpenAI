a36b77c20a0ab465e9fcf2ff5a1fe07c1f5d8da6
Hyunsoo Shin <2091126@hansung.ac.kr>
2024-05-24 23:35:26 +0900

    Feat. Google Auth.

--- 문제_내용_매체_선택.py
+++ 문제_내용_매체_선택.py
@@ -10,6 +10,32 @@ st.caption("Sponsored by")
 
 st.image('hsu.png', width=200)
 
+from streamlit_google_auth import Authenticate
+
+google_credentials = st.secrets["GOOGLE_CREDENTIALS"]
+
+st.title('Streamlit Google Auth Example')
+
+authenticator = Authenticate(
+    secret_credentials_path = 'google_credentials.json',
+    cookie_name='my_cookie_name',
+    cookie_key='this_is_secret',
+    redirect_uri = 'https://quiz-bot-4.streamlit.app/',
+)
+
+# Catch the login event
+authenticator.check_authentification()
+
+# Create the login button
+authenticator.login()
+
+if st.session_state['connected']:
+    st.image(st.session_state['user_info'].get('picture'))
+    st.write('Hello, '+ st.session_state['user_info'].get('name'))
+    st.write('Your email is '+ st.session_state['user_info'].get('email'))
+    if st.button('Log out'):
+        authenticator.logout()
+
 # Custom CSS for the footer
 footer_css = """
 <style>
